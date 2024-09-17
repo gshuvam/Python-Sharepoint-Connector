6ff30d07fabaecba8db45cd1ce5311ec20b86396
Shuvam Ghosh <shuvam1309@gmail.com>
2024-09-17 20:54:34 +0530

    Modify connector code to accomodate bearer token authentication

--- connector/sharepoint_connector.py
+++ connector/sharepoint_connector.py
@@ -2,17 +2,26 @@ import requests
 from logger.custom_logger import get_logger
 
 class SharePointConnector:
-    def __init__(self, site_url, cookie_dict):
+    def __init__(self, site_url, cookie_dict=None, auth_token=None):
         self.logger = get_logger(self.__class__.__name__)
         self.site_url = site_url
-        self.cookie_dict = cookie_dict 
+        self.cookie_dict = cookie_dict
+        self.auth_token = auth_token
         self.session = self.__create_session()
         self.digest_value = self.__get_form_digest_value(site_url)
 
     def __create_session(self):
         session = requests.Session()
-        for name, value in self.cookie_dict.items():
-            session.cookies.set(name, value)
+
+        if self.cookie_dict:
+            for name, value in self.cookie_dict.items():
+                session.cookies.set(name, value)
+        
+        if self.auth_token:
+            session.headers.update({
+                'Authorization': f'Bearer {self.auth_token}'
+            })
+
         return session
     
     def __get_form_digest_value(self, site_url):
